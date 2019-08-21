f9947184d3522272f7eafec1ed38467a4a1516b8
Frederik Dennig <frederik.dennig@uni-konstanz.de>
2019-08-21 20:22:08 +0200

    Added more tests

--- test/quadtree.test.ts
+++ test/quadtree.test.ts
@@ -8,5 +8,46 @@ test('retrieve', () => {
     let n = qt.retrieve(new Point(0, 10));
     expect(n).toBe(6);
     n = qt.retrieve(new Point(0, 5));
-    expect(n).toBe(undefined);
+    expect(n).toBeUndefined();
+});
+
+test('retrieve', () => {
+    const qt = Quadtree.createQuadtree<number>(100, 100);
+    qt.insert(new Rectangle(0, 10, 5, 5), 6);
+    let n = qt.retrieve(new Point(0, 10));
+    expect(n).toBe(6);
+    n = qt.retrieve(new Point(0, 5));
+    expect(n).toBeUndefined();
+});
+
+test('remove', () => {
+    const qt = Quadtree.createQuadtree<number>(100, 100);
+    qt.insert(new Rectangle(0, 10, 5, 5), 6);
+    let i = qt.remove(new Rectangle(0, 10, 5, 5));
+    expect(i).toBeDefined();
+    const n = i !== undefined ? i.object : undefined;
+    expect(n).toBe(6);
+    i = qt.remove(new Rectangle(0, 10, 5, 5));
+    expect(i).toBeUndefined();
+});
+
+test('remove object', () => {
+    const qt = Quadtree.createQuadtree<number>(100, 100);
+    qt.insert(new Rectangle(0, 10, 5, 5), 6);
+    const i = qt.removeObject(6);
+    expect(i).toBeDefined();
+    const n = i !== undefined ? i.object : undefined;
+    expect(n).toBe(6);
+    expect(qt.removeObject(6)).toBeUndefined();
+});
+
+test('clean', () => {
+    const qt = Quadtree.createQuadtree<number>(100, 100);
+    qt.insert(new Rectangle(0, 10, 5, 5), 6);
+    qt.insert(new Rectangle(1, 20, 10, 10), 6);
+    qt.insert(new Rectangle(40, 30, 5, 15), 6);
+    qt.clear();
+    expect(qt.retrieve(new Rectangle(0, 10, 5, 5))).toBeUndefined();
+    expect(qt.retrieve(new Rectangle(1, 20, 10, 10))).toBeUndefined();
+    expect(qt.retrieve(new Rectangle(40, 30, 5, 15))).toBeUndefined();
 });
--- test/rectangle.test.ts
+++ test/rectangle.test.ts
@@ -2,10 +2,18 @@ import { Rectangle } from '../src/rectangle';
 
 test('copy', () => {
     const r = new Rectangle(0, 1, 100, 200);
-	const c = r.copy();
+    const c = r.copy();
     expect(r === c).toBe(false);
     expect(c.x).toBe(r.x);
     expect(c.y).toBe(r.y);
     expect(c.width).toBe(r.width);
     expect(c.height).toBe(r.height);
+});
+
+test('equals', () => {
+    const r1 = new Rectangle(0, 1, 100, 200);
+    const r2 = new Rectangle(0, 1, 100, 200);
+    expect(r1.equals(r2)).toBe(true);
+    const r3 = new Rectangle(1, 0, 10, 200);
+    expect(r1.equals(r3)).toBe(false);
 });
